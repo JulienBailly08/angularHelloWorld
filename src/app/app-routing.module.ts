f2ff7f8ba7a2556a84372a9e71e46950a5660ec5
Julien_B <julienbailly08@gmail.com>
2021-07-30 15:15:54 +0200

    add 404 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { DriversListComponent } from './drivers-list/drivers-list.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 
 const routes: Routes = [
-  {path: 'cars',component: CarsListComponent},
-  {path: 'drivers',component: DriversListComponent}
+  { path: '', component: HomePageComponent},
+  { path: 'cars', component: CarsListComponent },
+  { path: 'drivers', component: DriversListComponent },
+  { path: '**', pathMatch: 'full', component:FourOhFourComponent}
+  
 ];
 
 @NgModule({
