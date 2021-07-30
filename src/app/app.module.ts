d87805f9418ae54e679ae95720dfa516e4c8656d
Julien_B <julienbailly08@gmail.com>
2021-07-30 13:43:22 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarsListComponent } from './cars-list/cars-list.component';
+import { DriversListComponent } from './drivers-list/drivers-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarsListComponent,
+    DriversListComponent
   ],
   imports: [
     BrowserModule,
