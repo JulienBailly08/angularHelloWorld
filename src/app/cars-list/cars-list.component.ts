7ff2721428b56585242c6b7ad2eed747be54ba5c
Julien_B <julienbailly08@gmail.com>
2021-08-03 16:12:32 +0200

    add cars infos

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -7,13 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsListComponent implements OnInit {
 
-  enfantUn:any = { name: "Mercedes AMG", price: 42000 };
-  enfantDeux:any = { name: "Range Rover Evoque", price: 55000 };
-  enfantTrois:any = { name: "Ferrari F50", price: 35000 };
+  carOne: any = {
+    name: "pagani huayra",
+    pays: "italie",
+    power: 765,
+    perf: 3.2
+  };
+  carTwo: any = {
+    name: "koenigsegg agera rs",
+    pays: "suéde",
+    power: 1383,
+    perf: 2.6
+  };
+  carThree: any = {
+    name: "zenvo tsr s",
+    pays: "danemark",
+    power: 1200,
+    perf: 2.8
+  };
+  carUpdate: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.carUpdate = new Date();
   }
 
 }
