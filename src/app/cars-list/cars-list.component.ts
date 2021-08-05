239d71ca10ff410b410ea21911e22ced7d59d4d7
Julien_B <julienbailly08@gmail.com>
2021-08-05 14:29:53 +0200

    creation tableau de dat et boucle for pour afficher voitures

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -7,26 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsListComponent implements OnInit {
 
-  carOne: any = {
-    name: "pagani huayra",
-    pays: "italie",
-    power: 765,
-    perf: 3.2
-  };
-  carTwo: any = {
-    name: "koenigsegg agera rs",
-    pays: "suéde",
-    power: 1383,
-    perf: 2.6
-  };
-  carThree: any = {
-    name: "zenvo tsr s",
-    pays: "danemark",
-    power: 1200,
-    perf: 2.8
-  };
-  
   carUpdate: any;
+
+
+  cars: any = [
+    {
+      name: "pagani huayra",
+      pays: "italie",
+      power: 765,
+      perf: 3.2
+    },
+    {
+      name: "koenigsegg agera rs",
+      pays: "suéde",
+      power: 1383,
+      perf: 2.6
+    },
+    {
+      name: "zenvo tsr s",
+      pays: "danemark",
+      power: 1200,
+      perf: 2.8
+    }
+  ]
+
+
 
   constructor() { }
 
