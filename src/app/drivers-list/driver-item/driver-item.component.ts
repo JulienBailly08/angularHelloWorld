a0c647a014fd8ecab7c1bec4b033c9542cf41ea3
Julien_B <julienbailly08@gmail.com>
2021-08-09 11:52:00 +0200

    allright execpt one girl

--- src/app/drivers-list/driver-item/driver-item.component.ts
+++ src/app/drivers-list/driver-item/driver-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-driver-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./driver-item.component.css']
 })
 export class DriverItemComponent implements OnInit {
+  @Input() driver: any;
 
   constructor() { }
 
